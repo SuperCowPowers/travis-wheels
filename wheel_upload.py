6e2087c124e55599315fd1dd97c95cc8cfc567f8
Brian Wylie <briford.wylie@gmail.com>
2014-08-17 04:55:46 -0600

    wheel upload first pass

--- wheel_upload.py
+++ wheel_upload.py
@@ -1,7 +1,19 @@
 
 """Push wheels up to S3 bucket"""
 import boto
+from boto.s3.key import Key
+
+_bucket = 'workbench-wheels'
+_key = 'py27/wheelhouse.tar.gz'
+
+# Spin up the S3 connection
 conn = boto.connect_s3()
-k = Key('workbench-wheels')
-k.key = 'py27/wheelhouse.tar.gz'
-k.set_contents_from_filename('wheelhouse.tar.gz')
+bucket = conn.get_bucket(_bucket)
+mykey = bucket.get_key(_key)
+if not mykey:
+    print 'Could not find key %s, creating a new file...' % _key
+    mykey = Key(bucket)
+    mykey.key = _key
+    
+# Upload the file
+mykey.set_contents_from_filename('wheelhouse.tar.gz')
